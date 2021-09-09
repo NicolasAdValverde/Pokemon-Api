6b9891747c43821c6f362a75ae6856ed8de4f077
Valverde Nicolas <valverdenicolasadrian@gmail.com>
2021-09-09 19:29:24 -0300

    se arreglo un error con el boton de busqueda

--- js/app.js
+++ js/app.js
@@ -28,11 +28,15 @@ $("#boton-busqueda").on("click", () => {
     $.get(`https://pokeapi.co/api/v2/pokemon/${busqueda}`, (res) => {
         $(".pokemones").html(
             `
-          <H1> ${res.name} </H1>
-          <img src=${res.sprites.other.dream_world.front_default} alt=""/>
-          <h3> EXP: ${res.base_experience} </h3>
-          <h3> ATAQUE: ${res.stats[1].base_stat} </h3>
-          <h3> DEFENSA: ${res.stats[2].base_stat} </h3>
+            <div class="card">
+            <H1> ${res.name} </H1>
+            <div class="card__img">
+            <img src=${res.sprites.other.dream_world.front_default} alt=""/>
+            </div>
+            <h3> EXP: ${res.base_experience} </h3>
+            <h3> ATAQUE: ${res.stats[1].base_stat} </h3>
+            <h3> DEFENSA: ${res.stats[2].base_stat} </h3>
+            </div>
         `
         );
     });
